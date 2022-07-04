27f925d5311b78043888a21b7b2e6b6a4ba1ef36
kimdonghyeon <sh2115521@naver.com>
2022-07-04 14:19:30 +0900

    전사가 무기를 여러개 다룰 수 있어야 합니다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,51 @@
-// 힌트 : if ( a무기 == null ) { ... }
-
 class Main {
     public static void main(String[] args) {
         전사 a전사 = new 전사();
+
+        a전사.a무기1 = new 칼();
         a전사.공격();
-        // 출력 => 아직 무기가 없습니다.
-        a전사.a무기 = new 칼();
+        // 출력 => 전사가 칼로 공격합니다.
+
+        a전사.a무기1 = new 활();
         a전사.공격();
-        // 출력 => 칼로 공격합니다.
+        // 출력 => 전사가 활로 공격합니다.
+
+        a전사.a무기2 = new 칼();
+        a전사.공격();
+        // 출력 => 전사가 활로 공격합니다.
+        // 출력 => 전사가 칼로 공격합니다.
     }
 }
 class 전사{
+    무기 a무기1;
+    무기 a무기2;
 
-    무기 a무기;
     void 공격(){
-        if(a무기 == null){
-            System.out.println("아직 무기가 없습니다.");
-        }else{
-            a무기.공격();
-        }
+        if(a무기1 !=null && a무기2 != null) {
+            a무기1.작동();
+            a무기2.작동();
+        }else if(a무기1 != null)
+            a무기1.작동();
+        else
+            a무기2.작동();
     }
+
 }
 
 abstract class 무기{
-    abstract void 공격();
+    abstract void 작동();
 }
+
 class 칼 extends 무기{
-    void 공격(){
-        System.out.println("칼로 공격합니다.");
+    @Override
+    void 작동() {
+        System.out.println("전사가 칼로 공격합니다.");
+    }
+}
+
+class 활 extends 무기{
+    @Override
+    void 작동() {
+        System.out.println("전사가 활로 공격합니다.");
     }
 }
