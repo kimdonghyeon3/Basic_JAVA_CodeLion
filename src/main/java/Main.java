5fcc2e31fdd6b0c8c3137a59016b3fcdd9a3bd4c
kimdonghyeon <sh2115521@naver.com>
2022-07-05 15:15:36 +0900

    사람을 만들 때 왼팔과 손과 엄지손가락이 한번에 만들어지게 해주세요 (생성자 4개 // 생성자 3개 // 생성자 1개)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,35 @@
-// 문제 : 아래와 같이 출력되게 해주세요.
-
 class Main {
     public static void main(String[] args) {
-        전사 a전사0 = new 전사();
-        a전사0.나이 = 20;
-        a전사0.이름 = "칸";
-        a전사0.성격 = "차가움";
-        a전사0.a무기 = new 활();
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
 
-        전사 a전사1 = new 전사();
-        전사 a전사2 = new 전사();
-        전사 a전사3 = new 전사();
-        전사 a전사4 = new 전사();
-        전사 a전사5 = new 전사();
-
-        System.out.println(a전사0.이름);
-        // 출력 : 칸
-        System.out.println(a전사1.이름);
-        // 출력 : No Name
-        System.out.println(a전사2.이름);
-        // 출력 : No Name
-        System.out.println(a전사3.이름);
-        // 출력 : No Name
-        System.out.println(a전사4.이름);
-        // 출력 : No Name
-        System.out.println(a전사5.이름);
-        // 출력 : No Name
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
     }
 }
 
-class 전사 {
-    int 나이;
-    String 이름;
-    String 성격;
-    무기 a무기;
-
-    // `전사()` 는 생성자 메서드 이다.
-    // 클래스 명과 같은 이름의 메서드를 생성자 메서드 라고 한다.
-    // 생성자 메서드는 자동으로 호출됩니다.
-    // 생성자 메서드는 new 할때(객체 생성할 때) 자동으로 호출됩니다.
-    // 생성자 메서드는 다른 모든 메서드 보다 가장 먼저 딱 1번 실행됩니다.
-    // 생성자 메서드는 리턴타입이 없습니다.
-    // 당연하지만 생성자 메서드는 리턴값도 없습니다.
-    전사() {
-        // 수정가능지역 시작
-        this.나이 = 20;
-        this.이름 = "No Name";
-        this.성격 = "온순함";
-        this.a무기 = new 칼();
-        // 수정가능지역 끝
+class 사람{
+    팔 a왼팔;
+    사람(){
+        a왼팔 = new 팔();
+        a왼팔.a손.a엄지손가락.길이 = 5;
     }
 }
-
-abstract class 무기 {
-
+class 팔{
+    static 손 a손;
+//    팔(){
+//        a손 = new 손();
+//    }
 }
-
-class 칼 extends 무기{}
-class 활 extends 무기{}
+class 손{
+    static 손가락 a엄지손가락;
+//    손(){
+//        a엄지손가락 = new 손가락();
+//        a엄지손가락.길이=10;
+//    }
+}
+class 손가락{
+    static int 길이;
+//    손가락(){
+//        길이 = 5;
+//    }
+}
