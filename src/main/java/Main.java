d59db35e4bc88eb02689a376db42206657cb3037
kimdonghyeon <sh2115521@naver.com>
2022-07-06 17:26:22 +0900

    stream 활용 다항식 계산하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.stream.*;
 class Main {
     public static void main(String[] args) {
         String line = "Test 15 lqlq 35 bad 99999 guess 34";
+        String s = "30 + 40 + 20 + 40 + 20 + 50 + 11 + -6  + -100";
         // s["Test" "15" "lqlq" "35" "bad" "99999" "guess" "34"]
         // s["15" "35" "99999" "34"]
         // s[15 35 99999 34]
@@ -15,15 +16,21 @@ class Main {
 //                .mapToInt(Integer::parseInt)
 //                .sum();
 
+        int sum1 = Arrays.stream(s.split(" "))
+                .filter((ss) -> ss.matches("\\d+")||ss.matches("-\\d+"))
+                .mapToInt(Integer::parseInt)
+                .sum();
+
         int sum = 0;
         String[] str = line.split(" ");
         for(int i = 0 ; i < str.length ; i++){
-            if(str[i].matches("\\d+")){
+            if(str[i].matches("\\d+|-\\d+")){
                 sum += Integer.parseInt(str[i]);
             }
         }
 
 
         System.out.println(sum);
+        System.out.println("sum1 = " + sum1);
     }
 }
