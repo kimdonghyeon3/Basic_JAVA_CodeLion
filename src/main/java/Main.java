460087817339769dd9d6d258391d3036e651ae6f
kimdonghyeon <sh2115521@naver.com>
2022-07-07 13:41:24 +0900

    묵시적 super의 활용 예제 2 (자식에게서 이미 override된 메서의 경우 super를 통해 상위 클래스에서 호출한다면, 자신의 메서드로 실행이된다)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,30 @@
-// 문제 : 아래가 오류없이 실행되도록 해주세요.
-// 조건 : 사람 클래스만 수정할 수 있습니다.
+//code.oa.gg/java8/938
+// 문제 : 아래와 같이 출력되도록 해주세요. init() 메세드에 의해서 출력되어야 합니다.
+// 조건 : 사람 클래스의 생성자는 사용할 수 없습니다.
+// 힌트 : 모든 생성자는 자신의 일을 하기 전에 super(); 부터 실행합니다.
+// 힌트 : 모든 생성자는 자신의 일을 하기 전에 부모클래스의 생성자 부터 실행합니다.
 
 class Main {
     public static void main(String[] args) {
         new 사람();
+        // 출력 : 사람이 초기화 됩니다.
     }
 }
 
 class 동물 {
-    동물(String 이름) {
-        System.out.println("동물(String 이름) 실행됨!");
-        System.out.println("이름 : " + 이름);
+
+    동물(){
+        init();
     }
-    동물(String 이름, int 나이) {
-        System.out.println("동물(String 이름, int 나이) 실행됨!");
-        System.out.println("이름 : " + 이름);
-        System.out.println("나이 : " + 나이);
+
+    void init() {
+
     }
 }
 
 class 사람 extends 동물 {
-    사람(){
-        super("홍길동",22);
+
+    void init() {
+        System.out.println("사람이 초기화 됩니다.");
     }
 }
