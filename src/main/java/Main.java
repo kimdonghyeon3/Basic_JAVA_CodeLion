a6848bb33be8666ad12ad486381ddae338780ad8
kimdonghyeon <sh2115521@naver.com>
2022-07-08 11:45:57 +0900

    static의 이해

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,70 +1,34 @@
-// 문제 : 아래와 같이 출력 되도록 전사A, 전사B를 만들어주세요.
-// 조건 : this와 super를 활용해주세요.
+//code.oa.gg/java8/948
+// 문제 : 아래 코드의 잘 못된 점을 고쳐보세요.
 
 class Main {
     public static void main(String[] args) {
-        전사A a전사A = new 전사A();
-        a전사A.이름 = "홍길동";
+        사람 a사람1 = new 사람();
+        a사람1.이름 = "홍길동";
+        a사람1.나이 = 22;
 
-        System.out.println("== 전사A 공격(); ==");
-        a전사A.공격();
-        // 출력
-        // 홍길동이(가) 공격!
-        // 사람이(가) 숨쉬다.
+        a사람1.자기소개();
+        // 출력 : 저는 22살 홍길동 입니다.
 
-        System.out.println("\n== 전사A 공격2(); ==");
-        a전사A.공격2();
-        // 홍길동이(가) 공격!
-        // 사람이(가) 숨쉬다.
+        사람 a사람2 = new 사람();
+        a사람2.이름 = "홍길순";
+        a사람2.나이 = 25;
 
-        전사B a전사B = new 전사B();
-        a전사B.이름 = "홍길순";
+        a사람2.자기소개();
+        // 출력 : 저는 25살 홍길순 입니다.
 
-        System.out.println("\n== 전사B 공격(); ==");
-        a전사B.공격();
-        // 홍길순이(가) 공격!
-        // 전사B가 숨쉬다.
-
-        System.out.println("\n== 전사B 공격2(); ==");
-        a전사B.공격2();
-        // null이(가) 공격!
-        // 사람이(가) 숨쉬다.
+        a사람1.이름 = "홍길동";
+        a사람1.나이 = 22;
+        a사람1.자기소개();
+        // 출력 : 저는 22살 홍길동 입니다.
     }
 }
 
 class 사람 {
-    String 이름;
+    static int 나이;
+    static String 이름;
 
-    void 숨쉬다() {
-        System.out.println("사람이 숨쉬다.");
-    }
-}
-
-class 전사 extends 사람{
-    void 공격(){
-        System.out.println(이름 + "이 공격!!");
-        숨쉬다();
-    }
-    void 공격2() {
-        System.out.println(이름 + "이 공격!!");
-        super.숨쉬다();
-    }
-}
-
-class 전사A extends 전사{
-
-}
-
-class 전사B extends 전사{
-
-    String 이름;
-
-    void 공격(){
-        System.out.println(이름 + "이 공격!!");
-        숨쉬다();
-    }
-
-    void 숨쉬다() {
-        System.out.println("전사B가 숨쉬다.");
+    static void 자기소개() {
+        System.out.println("저는 " + 나이 + "살 " + 이름 + " 입니다.");
     }
 }
