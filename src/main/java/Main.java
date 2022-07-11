71b20665966fdeb7b9d9e871dd61a10c12154d0a
kimdonghyeon <sh2115521@naver.com>
2022-07-11 11:35:13 +0900

    overload 이해

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,80 @@
-//code.oa.gg/java8/948
-// 문제 : 아래 코드의 잘 못된 점을 고쳐보세요.
+//code.oa.gg/java8/958
+// 가정 : 우리는 오라클 자바 개발팀에 취업을 했습니다. 우리는 자바 10의 새로운 기능은 `저장소` 클래스를 만들어야 합니다.
+// 다음 코드가 오류없이 진행되도록 해주세요. 오류만 안나면 됩니다.
 
 class Main {
     public static void main(String[] args) {
-        사람 a사람1 = new 사람();
-        a사람1.이름 = "홍길동";
-        a사람1.나이 = 22;
+        저장소.저장(10);
+        저장소.저장("안녕");
+        저장소.저장(new 사람());
+        저장소.저장(new 사과());
+        저장소.저장(5.5);
+        저장소.저장(false);
+        저장소.저장('a');
 
-        a사람1.자기소개();
-        // 출력 : 저는 22살 홍길동 입니다.
+        // 정수 계열
+        byte b = 0; // 1바이트
+        char c = 1; // 2바이트
+        short s = 2; // 2바이트
+        int i = 3; // 4바이트
+        long l = 4; // 8바이트
 
-        사람 a사람2 = new 사람();
-        a사람2.이름 = "홍길순";
-        a사람2.나이 = 25;
+        // 실수 계열
+        // 4.1f => float 형 실수를 의미
+        float f = 3.14f; // 4바이트
+        // 3.141592 => double 형 실수를 의미
+        double d = 3.141592; // 8 바이트
 
-        a사람2.자기소개();
-        // 출력 : 저는 25살 홍길순 입니다.
+        // 논리 계열
+        boolean bl = false; // 1/8 바이트, 1비트
 
-        a사람1.이름 = "홍길동";
-        a사람1.나이 = 22;
-        a사람1.자기소개();
-        // 출력 : 저는 22살 홍길동 입니다.
+        저장소.저장(b);
+        저장소.저장(s);
+        저장소.저장(c);
+        저장소.저장(i);
+        저장소.저장(l);
+        저장소.저장(f);
+        저장소.저장(d);
+        저장소.저장(bl);
+    }
+}
+class 저장소 {
+    static void 저장(byte v) {
+    }
+
+    static void 저장(short v) {
+    }
+
+    static void 저장(char v) {
+    }
+
+    static void 저장(int v) {
+    }
+
+    static void 저장(long v) {
+    }
+
+    static void 저장(float v) {
+    }
+
+    static void 저장(double v) {
+    }
+
+    static void 저장(boolean v) {
+    }
+
+    static void 저장(사과 v) {
+    }
+
+    static void 저장(사람 v) {
+    }
+
+    static void 저장(String v) {
     }
 }
 
-class 사람 {
-    static int 나이;
-    static String 이름;
+class 사과 {
+}
 
-    static void 자기소개() {
-        System.out.println("저는 " + 나이 + "살 " + 이름 + " 입니다.");
-    }
+class 사람 {
 }
