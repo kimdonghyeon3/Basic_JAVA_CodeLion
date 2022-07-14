428f323f148bac7234edfc23a8b479800ff52510
kimdonghyeon <sh2115521@naver.com>
2022-07-14 09:43:31 +0900

    ArrayList 만들기 문제 2(개념 - add 함수)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,40 @@ class Main {
         ar.add(100);
         ar.add(200);
         ar.add(300);
+
+        int value = ar.get(0);
+        System.out.println(value);
+        // 출력 : 100
+
+        value = ar.get(1);
+        System.out.println(value);
+        // 출력 : 200
+
+        value = ar.get(2);
+        System.out.println(value);
+        // 출력 : 300
     }
 }
 
-class ArrayList{
 
-    void add(int num){
 
+class ArrayList {
+
+    int[] data;
+    int index;
+
+    ArrayList() {
+        data = new int[3];
+        index = 0;
+    }
+
+
+    void add(int data) {
+        this.data[index++] = data;
+    }
+
+    int get(int index){
+
+        return data[index];
     }
 }
