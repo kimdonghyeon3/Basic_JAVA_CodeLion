b460aaa239eebc6ffb988e51ecbfe59f9327d1b9
kimdonghyeon <sh2115521@naver.com>
2022-07-18 10:45:21 +0900

    제너릭이 적용된 ArrayList 클래스를 만들어주세요. 처음부터 끝까지

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,131 @@
+// ArrayList 만들기
+// add => 추가[완료]
+// addAt => 추가
+// indexOf => 검색[완료]
+// contains => 있는지 없는지[완료]
+// get => 가져오는 거[완료]
+// remove => 삭제[완료]
+// toString => 내부 데이터 표시[완료]
+// 제너릭
+
 class Main {
     public static void main(String[] args) {
-        // 여기서 `저장소<>` 는 `저장소<Integer>` 가 생략된 표현이다.
-        저장소<Integer> a정수저장소 = new 저장소<>();
-        a정수저장소.set(10);
-        System.out.println(a정수저장소.get() * a정수저장소.get());
-        // 출력 => 100
+        ArrayList<Integer> al = new ArrayList<Integer>();
 
-        저장소<Boolean> a논리저장소 = new 저장소<>();
-        a논리저장소.set(false);
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
 
-        if ( a논리저장소.get() ) {
-            System.out.println("참 : " + a논리저장소.get());
+        al.add(50, 1);
+
+        al.remove(0);
+
+        System.out.println(al);
+
+        System.out.println(al.get(3));
+
+        System.out.println(al.indexOf(50));
+        System.out.println(al.contains(40));
+
+        System.out.println(al.get(3) + al.get(3));
+
+        ArrayList<Double> al2 = new ArrayList<Double>();
+        al2.add(10.5);
+        al2.add(3.14);
+        System.out.println(al2);
+    }
+}
+class ArrayList<T> {
+    private Object[] datas;
+    private int size;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for ( int i = 0; i < size; i++ ) {
+            if ( i > 0 ){
+                sb.append(", ");
+            }
+            sb.append("[" + i + " : " + datas[i] + "]");
         }
-        else {
-            System.out.println("거짓 : " + a논리저장소.get());
+
+        sb.insert(0, "ArrayList => (");
+        sb.append("), size : " + size);
+
+        return sb.toString();
+    }
+
+    ArrayList() {
+        datas = new Object[3];
+        size = 0;
+    }
+
+    private void sizeUpDatasSizeIfFull() {
+        if ( isFull() ) {
+            sizeUpDatas();
         }
+    }
+
+    private boolean isFull() {
+        return size == datas.length;
+    }
+
+    private void sizeUpDatas() {
+        Object[] newDatas = new Object[datas.length * 2];
+
+        for ( int i = 0; i < size; i++ ) {
+            newDatas[i] = datas[i];
+        }
+
+        datas = newDatas;
+    }
+
+    public void add(T data) {
+        sizeUpDatasSizeIfFull();
+
+        datas[size] = data;
+        size++;
+    }
+
+    public void add(T data, int index) {
+        sizeUpDatasSizeIfFull();
+
+        for ( int i = size - 1; i >= index; i-- ) {
+            datas[i + 1] = datas[i];
+        }
+
+        datas[index] = data;
+        size++;
+    }
+
+    public void remove(int index) {
+        sizeUpDatasSizeIfFull();
+
+        for ( int i = index; i < size; i++ ) {
+            datas[i] = datas[i + 1];
+        }
+
+        size--;
+    }
+
+    public T get(int index) {
+        return (T)datas[index];
+    }
+
+    public int indexOf(Object data) {
+        for ( int i = 0; i < size; i++ ) {
+            if ( datas[i].equals(data) ) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean contains(Object data) {
+        return indexOf(data) != -1;
     }
 }
 
-class 저장소<T>{
-
-    T t;
-
-    public void set(T n){
-        t = n;
-    }
-
-    public T get() {
-        return t;
-    }
-}
