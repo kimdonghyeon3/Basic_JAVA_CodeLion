0eee090cb60b2025e423a9d5f316698552a9ffc0
kimdonghyeon <sh2115521@naver.com>
2022-07-18 11:06:10 +0900

    HashMap 만들기 문제 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,53 @@
-//code.oa.gg/java8/1127
+//code.oa.gg/java8/1131
+// 문제 : 아래와 같이 출력되도록 해주세요.
+
 class Main {
     public static void main(String[] args) {
         HashMap aMap = new HashMap();
 
         aMap.put(1, "안녕");
         aMap.put("A", false);
+        aMap.put("A", true);
+
+        System.out.println(aMap.get("A"));
+        // 출력 : true
     }
 }
 
-class HashMap{
+class HashMap {
+    private Object[] keys;
+    private Object[] datas;
+    int size = 0;
 
-    public void put(int i, String 안녕) {
+    HashMap() {
+        keys = new Object[100];
+        datas = new Object[100];
     }
 
-    public void put(String s, boolean b) {
+    void put(Object key, Object data) {
+        keys[size] = key;
+        datas[size] = data;
+        size++;
+    }
+
+    Object get(Object key) {
+        Object v = null;
+        int index = -1;
+
+        for ( int i = 0; i < size; i++ ) {
+            if ( key.equals(keys[i]) ) {
+                index = i;
+                break;
+            }
+        }
+
+        if ( index != -1 ) {
+            v = datas[index];
+        }
+
+        return v;
     }
 }
+
+
+
