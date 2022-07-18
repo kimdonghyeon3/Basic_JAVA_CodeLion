74aebb01b799db49d8467c35865ef8168bc31d56
kimdonghyeon <sh2115521@naver.com>
2022-07-18 15:18:32 +0900

    인터페이스 문제 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
-//code.oa.gg/java8/894
+//code.oa.gg/java8/896
 // 문제 : 아래가 실행되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
         사람 a사람 = new 홍길동();
         변호사 a변호사 = (변호사)a사람;
+        치과의사 a치과의사 = (치과의사)a사람;
+        성화봉송자 a성화봉송자 = (성화봉송자)a사람;
     }
 }
-
-class 사람{ }
-class 홍길동 extends 사람 implements 변호사{}
+class 사람{}
 interface 변호사{}
+interface 치과의사{}
+interface 성화봉송자{}
+class 홍길동 extends 사람 implements 변호사, 치과의사, 성화봉송자{}
