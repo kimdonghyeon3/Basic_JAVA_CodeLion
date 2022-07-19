d7fea8c4ecd5053faa8e027fc4776afd623409f7
kimdonghyeon <sh2115521@naver.com>
2022-07-19 11:19:23 +0900

    throws exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,17 @@
-//code.oa.gg/java8/1346
-// 문제 : 아래가 실행되도록 해주세요.
-
 class Main {
-    public static void main(String[] args) {
-        사람 a사람 = new 사람();
+    public static void main(String[] args){
 
-        변호사 a변호사 = a사람;
-        a변호사.변호하다();
-        // 출력 : 사람이 변호 합니다.
+        try{
+            int rs = 계산기.나누다(10, 0);
+            System.out.println(rs);
+        }catch(ArithmeticException e){
+            System.out.println("0으로 나누면 안됩니다.");
+        }
 
-        변호사 a변호사2 = new 오랑우탄();
-        a변호사2.변호하다();
-        // 출력 : 오랑우탄이 변호 합니다.
-
-        의사 a의사 = new 오랑우탄();
-        a의사.진찰하다();
-        // 출력 : 오랑우탄이 진찰 합니다.
-
-        의사 a의사2 = new 사람();
-        a의사2.진찰하다();
-        // 출력 : 사람이 진찰 합니다.
     }
 }
-
-interface 의사 {
-    public void 진찰하다();
-}
-
-interface 변호사 {
-    public void 변호하다();
-}
-
-class 사람 implements 의사, 변호사 {
-    public void 변호하다() {
-        System.out.println("사람이 변호 합니다.");
-    }
-
-    public void 진찰하다() {
-        System.out.println("사람이 진찰 합니다.");
-    }
-}
-
-class 오랑우탄 implements 의사, 변호사 {
-    public void 변호하다() {
-        System.out.println("오랑우탄이 변호 합니다.");
-    }
-
-    public void 진찰하다() {
-        System.out.println("오랑우탄이 진찰 합니다.");
+class 계산기 {
+    static int 나누다(int a, int b) throws ArithmeticException {
+            return a / b;
     }
 }
