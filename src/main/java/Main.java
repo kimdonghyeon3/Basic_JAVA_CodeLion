99e23a4299fa6478c1136a0d454b1d822cd1bb7c
kimdonghyeon <sh2115521@naver.com>
2022-07-19 14:02:40 +0900

    피보나치 수열에서 5천만 이하이면서 짝수인 항의 합은 얼마인가요?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,29 @@
 class Main {
     public static void main(String[] args) {
 
-        long sum = 0;
+        //속도를 빠르게 하려면 dp를 사용하는 것도 좋아보인다
+        int sum = 0;
 
-        for(int i = 1 ; i < 100000 ; i++){
-            if(i % 3 == 0 || i % 5 == 0)
-                sum += i;
+        for(int i = 0 ; true ; i++){
+            int fiboNum = fibo(i);
+
+            if(fiboNum > 50000000)
+                break;
+
+            if(fiboNum % 2 == 0)
+                sum += fiboNum;
+
+//            System.out.println("fibo(i) = " + fiboNum);
         }
 
         System.out.println("sum = " + sum);
+    }
+
+    public static int fibo(int n){
+
+        if(n == 1 || n==0)
+            return n == 1 ? 1 : 0;
+
+        return fibo(n-1) + fibo(n-2);
     }
 }
