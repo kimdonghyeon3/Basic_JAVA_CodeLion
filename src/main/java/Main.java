e1b1eaafbd0d0150ad7121cc20aa3b43455d24a4
kimdonghyeon <sh2115521@naver.com>
2022-07-19 11:45:42 +0900

    getter setter 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,29 @@
+//code.oa.gg/java8/1071
+// 문제 : 사람의 id를 출력해주세요.
+// 조건 : 사람의 id를 20으로 설정해주세요.
+// 조건 : 사람 클래스의 id 필드의 private 속성은 수정할 수 없습니다.
 class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
 
-        try{
-            int rs = 계산기.나누다(10, 0);
-            System.out.println(rs);
-        }catch(ArithmeticException e){
-            System.out.println("0으로 나누면 안됩니다.");
-        }
-
+        // 구현시작
+        a사람.setId(20);
+        // 구현끝
+        // 출력 : 제 번호는 20 입니다.
+        System.out.println("제 번호는 " + a사람.getId() + " 입니다.");
     }
 }
-class 계산기 {
-    static int 나누다(int a, int b) throws ArithmeticException {
-            return a / b;
+
+class 사람 {
+    private int id;
+
+    // 힌트 : 여기에 메소드 2개 추가해야 합니다.
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
