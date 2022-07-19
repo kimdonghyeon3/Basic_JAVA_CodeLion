34e6cab4476e43300aa11c51251c8f0aa707c9c6
kimdonghyeon <sh2115521@naver.com>
2022-07-19 14:02:12 +0900

    100000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마인가요?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,13 @@
-//code.oa.gg/java8/1071
-// 문제 : 사람의 id를 출력해주세요.
-// 조건 : 사람의 id를 20으로 설정해주세요.
-// 조건 : 사람 클래스의 id 필드의 private 속성은 수정할 수 없습니다.
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
 
-        // 구현시작
-        a사람.setId(20);
-        // 구현끝
-        // 출력 : 제 번호는 20 입니다.
-        System.out.println("제 번호는 " + a사람.getId() + " 입니다.");
-    }
-}
+        long sum = 0;
 
-class 사람 {
-    private int id;
+        for(int i = 1 ; i < 100000 ; i++){
+            if(i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
 
-    // 힌트 : 여기에 메소드 2개 추가해야 합니다.
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        System.out.println("sum = " + sum);
     }
 }
