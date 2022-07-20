5770fb3b71981fb7d9b3cf7d9a035d625df9c587
kimdonghyeon <sh2115521@naver.com>
2022-07-20 10:20:40 +0900

    싱글톤 패턴, 문제 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,27 @@ class Main {
         사람들[3] = 사람.get사람();
         사람들[4] = 사람.get사람();
 
-        System.out.println("실행완료");
+        for ( int i = 0; i < 사람들.length; i++ ) {
+            사람들[i].자기소개();
+        }
+
     }
 }
 class 사람{
-    static 사람 사람 = new 사람();
+    static 사람 사람;
+    int id = 0;
+    static int num = 1;
+
+    public 사람(int i) {
+        id = i;
+    }
 
     public static 사람 get사람() {
-        return 사람;
+        사람 = new 사람(num++);
+;        return 사람;
+    }
+
+    public void 자기소개() {
+        System.out.println("저는 " + id +"번째 사람입니다.");
     }
 }
