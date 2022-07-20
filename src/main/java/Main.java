e175c5de2355bb29fc92d4dcd177a466e4f3f476
kimdonghyeon <sh2115521@naver.com>
2022-07-20 09:47:25 +0900

    싱글톤 패턴, 문제 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,36 @@
+//code.oa.gg/java8/1073
+// 문제 : 사람객체를 생성한 후 나이를 22살로 만들어주세요.
+// 조건 : 사람 생성자는 수정/추가할 수 없습니다.
+// 조건 : 아래와 같이 출력 되어야 합니다.
 class Main {
-
     public static void main(String[] args) {
+        사람 a사람;
 
-        long result = 0;
+        a사람 = 사람.getInstance();
+        a사람.set나이(22);
 
-        out : for(int i = 9999 ; i > 999 ; i--){
-            for(int j = 9999 ; j > 999 ; j--){
-                if(isSymmetry(i*j)){
-                    result = Math.max(result, i*j);
-//                    break out;    답이 나오긴 하는데,, 이건 오류가 발생할 수도 있겠다
-                }
-            }
-        }
+        System.out.println("사람의 나이는 " + a사람.get나이() + "살 입니다.");
+        // 출력 : 사람의 나이는 22살 입니다.
+    }
+}
 
-        System.out.println("result = " + ((result == -1) ? "대칭수가 없습니다.": result) );
+class 사람 {
+    private int 나이;
+
+    private 사람() {
     }
 
-    public static boolean isSymmetry(long n){
+    // 힌트 : 여기에 메서드 3개 추가해야 합니다.
 
-        String str = String.valueOf(n);
-
-        for(int i = 0 ; i < str.length()/2 ; i++){
-            if(str.charAt(i) != str.charAt(str.length()-i-1)){
-                return false;
-            }
-        }
-        return true;
+    public int get나이() {
+        return 나이;
     }
 
+    public void set나이(int 나이) {
+        this.나이 = 나이;
+    }
+
+    public static 사람 getInstance(){
+        return new 사람();
+    }
 }
