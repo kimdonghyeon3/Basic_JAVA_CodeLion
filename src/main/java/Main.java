6c7fb03f96f8555ab2ff61338b4a66fadcee2e7a
kimdonghyeon <sh2115521@naver.com>
2022-07-21 10:48:43 +0900

    버튼이 동물에게 클릭 사실을 알리도록 해주세요. Object 사용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,51 @@
-/*
-문제 : 사람 객체의 `안녕`이 `button.click();`에 의해 실행되도록 해주세요.
-조건 : Button에는 `사람` 이라는 언급이 없어야 합니다.
-*/
 class Main {
     public static void main(String[] args) {
-        Button button = new Button();
-        // button.?? // 이 1줄을 완성해주세요.(메인 클래스는 여기만 수정 가능)
-        button.setButton(new 사람());
-        button.click();
+        Button aButton = new Button();
+        aButton.setClickEventListener(new 고양이());
+        aButton.fireClick();
+        // 나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+        aButton.setClickEventListener(new 개());
+        aButton.fireClick();
+        // 나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+        aButton.setClickEventListener(new 부엉이());
+        // 나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+        aButton.fireClick();
     }
 }
-
-interface Listener{
-    void listen();
-}
-
 class Button{
 
-    Listener obj;
+    Object obj;
 
-    public void setButton(Listener listen) {
-        this.obj = listen;
+    public void fireClick() {
+        if(obj instanceof 고양이){
+            ((고양이)obj).listen();
+        }else if(obj instanceof 개){
+            ((개)obj).listen();
+        }else if(obj instanceof 부엉이){
+            ((부엉이)obj).listen();
+        }
     }
 
-    void click(){
-        obj.listen();
+    public void setClickEventListener(Object obj) {
+        this.obj = obj;
     }
 }
 
-class 사람 implements Listener{
-    public void 안녕() {
-        System.out.println("Main::안녕!");
+class 고양이{
+    void listen(){
+        System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
-
-    @Override
-    public void listen() {
-        안녕();
+}
+class 개{
+    void listen(){
+        System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
+    }
+}
+class 부엉이{
+    void listen(){
+        System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
     }
 }
