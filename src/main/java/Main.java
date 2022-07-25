10ed423cd1430fd9a896f721ccdec760ba6f5151
kimdonghyeon <sh2115521@naver.com>
2022-07-25 10:05:16 +0900

    내부 클래스

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,26 @@
 class Main {
     public static void main(String[] args) {
-        Button aButton = new Button();
-        aButton.setClickEventListener(new 고양이());
-        aButton.fireClick();
-        // 나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+        View.OnClickListener aOnClickListener;
 
-        aButton.setClickEventListener(new 개());
-        aButton.fireClick();
-        // 나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+        // 수정가능지역 시작
+        class 리스너1 implements View.OnClickListener {
+            @Override
+            public void onClick() {
+                System.out.println("클릭되었다는 사실을 전달받았습니다.");
+            }
+        }
 
-        aButton.setClickEventListener(new 부엉이());
-        // 나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+        aOnClickListener = new 리스너1();
+        // 수정가능지역 끝
 
-        aButton.fireClick();
-    }
-}
-class Button{
-
-    Listener obj;
-
-    public void fireClick() {
-        obj.listen();
-    }
-
-    public void setClickEventListener(Listener obj) {
-        this.obj = obj;
+        aOnClickListener.onClick();
+        // 출력 => 클릭되었다는 사실을 전달받았습니다.
     }
 }
 
-interface Listener{
-    void listen();
+class View {
+    static interface OnClickListener {
+        public void onClick();
+    }
 }
 
-class 고양이 implements Listener{
-    public void listen(){
-        System.out.println("나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
-    }
-}
-class 개 implements Listener{
-    public void listen(){
-        System.out.println("나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
-    }
-}
-class 부엉이 implements Listener{
-    public void listen(){
-        System.out.println("나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.");
-    }
-}
