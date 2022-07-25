841f7fab9f824821bc29e29990d2e073e2eec0f2
kimdonghyeon <sh2115521@naver.com>
2022-07-25 17:28:17 +0900

    람다 표현식

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,19 @@
 class Main {
     public static void main(String[] args) {
-        계산기 a계산기 = new 계산기();
-        a계산기.num1 = 10;
-        a계산기.num2 = 20;
+        View.OnClickListener aOnClickListener;
 
-        int 결과1 = a계산기.수행((num1, num2) -> num1+num2);
-        System.out.println(결과1); // 30
+        // 수정가능지역 시작
+        aOnClickListener = () -> System.out.println("클릭되었다는 사실을 전달받았습니다.");
 
-        int 결과2 = a계산기.수행((num1, num2) -> num1-num2);
-        System.out.println(결과2); // -10
+        // 수정가능지역 끝
 
-        int 결과3 = a계산기.수행((num1, num2) -> num1*num2);
-        System.out.println(결과3); // 300
-    }
-}
-class 계산기 {
-    int num1;
-    int num2;
-
-    int 수행(식 a식) {
-        return a식.실행(num1, num2);
+        aOnClickListener.onClick();
+        // 출력 => 클릭되었다는 사실을 전달받았습니다.
     }
 }
 
-interface 식 {
-    public int 실행(int num1, int num2);
+class View {
+    static interface OnClickListener {
+        public void onClick();
+    }
 }
