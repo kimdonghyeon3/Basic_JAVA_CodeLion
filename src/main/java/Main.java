8be52866073beea625b5a6e76eb4fae3acd9809a
kimdonghyeon <sh2115521@naver.com>
2022-07-25 11:20:46 +0900

    익명 클래스 v2 계산기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,13 @@ class Main {
         a계산기.num1 = 10;
         a계산기.num2 = 20;
 
-        int 결과1 = a계산기.수행(new 식(){
-            @Override
-            public int 실행(int num1, int num2) {
-                return num1 + num2;
-            }
-        });
+        int 결과1 = a계산기.수행((num1, num2) -> num1+num2);
         System.out.println(결과1); // 30
 
-        int 결과2 = a계산기.수행(new 식(){
-            @Override
-            public int 실행(int num1, int num2) {
-                return num1 - num2;
-            }
-        });
+        int 결과2 = a계산기.수행((num1, num2) -> num1-num2);
         System.out.println(결과2); // -10
 
-        int 결과3 = a계산기.수행(new 식(){
-            @Override
-            public int 실행(int num1, int num2) {
-                return num1 * num2;
-            }
-        });
+        int 결과3 = a계산기.수행((num1, num2) -> num1*num2);
         System.out.println(결과3); // 300
     }
 }
