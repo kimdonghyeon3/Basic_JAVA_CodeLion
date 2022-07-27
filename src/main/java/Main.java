940496743d0fc6bed3dd22a7365da24b0daef5c7
kimdonghyeon <sh2115521@naver.com>
2022-07-27 09:44:28 +0900

    setOnClickListener 메서드 구현, 작업 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,30 +10,26 @@ abstract class Activity {
     abstract public void onCreate();
 }
 
-class MainActivity extends Activity implements View.OnClickListener{
+class MainActivity extends Activity {
     @Override
     public void onCreate() {
         System.out.println("메인 액티비티가 실행되었습니다.");
 
         // 아래 코드가 실행되도록 해주세요.
         View aButton = new Button(this);
-        aButton.setOnClickListener(this);
+        aButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void clicked() {
+                System.out.println("익명내부객체는 버튼이 클릭되었다는 사실을 방금 알았습니다.");
+            } /* 이 부분을 구현해주세요. */ });
 
         aButton.fireClickEvent();
-        // 출력 : MainActivity는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
-        // 조건 : 위 문구는 MainActivity 클래스 안의 메서드에 의해서 출력되어야 합니다.
-    }
-
-
-    @Override
-    public void clicked() {
-        System.out.println("메인 엑티비티가 클릭되었습니다.");
+        // 출력 : 익명내부객체는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
     }
 }
 
 abstract class View {
     private OnClickListener onClickListener;
-    private Activity activity;
 
     static interface OnClickListener {
         public void clicked();
@@ -51,11 +47,7 @@ abstract class View {
     }
 }
 
-class Button extends View{
-
+class Button extends View {
     Button(Activity activity) {
     }
 }
-
-
-
