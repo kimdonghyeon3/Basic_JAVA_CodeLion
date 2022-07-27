8a2b8314395cb51c2c58e524d43f0755040c3f43
kimdonghyeon <sh2115521@naver.com>
2022-07-27 09:34:24 +0900

    setOnClickListener 메서드 구현, 작업 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,31 +19,50 @@ class MainActivity extends Activity {
         View.OnClickListener a = new 사과();
         View aButton = new Button(this);
         aButton.setOnClickListener(a);
+
+        aButton.fireClickEvent();
+        // 출력 : 사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
+        // 조건 : 위 문구는 사과 클래스 안의 메서드에 의해서 출력되어야 합니다.
     }
 }
 
 abstract class View {
-    public void setOnClickListener(OnClickListener a) {
+    public void fireClickEvent() {
     }
 
     static interface OnClickListener {
+
+        void fireClickEvent();
+    }
+
+    void setOnClickListener(OnClickListener onClickListener) {
+
     }
 }
 
-class 사과 implements View.OnClickListener{
+class Button extends View {
 
-}
+    OnClickListener onClickListener;
 
-class Button extends View{
-    Activity a;
+    Button(Activity activity) {
+    }
 
-    public Button(Activity a) {
-        this.a = a;
+    @Override
+    void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    @Override
+    public void fireClickEvent() {
+        onClickListener.fireClickEvent();
     }
 }
 
-
-
-
+class 사과 implements View.OnClickListener {
+    @Override
+    public void fireClickEvent() {
+        System.out.println("사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
+    }
+}
 
 
