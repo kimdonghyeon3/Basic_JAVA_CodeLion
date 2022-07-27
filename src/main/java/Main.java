0c6dd55e841570dcef2e45455f1ac43af4be1753
kimdonghyeon <sh2115521@naver.com>
2022-07-27 09:24:35 +0900

    액티비티 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,19 @@
-// 이번 작업 : 구독자 => OnClickListener 로 용어 변경
-// OnClickListener => 클릭 구독자
-// 클릭 구독자 => 누군가가 클릭되었다는 사실을 관찰하는 객체
-// 클릭 구독자 => 누군가가 클릭되었다면, 그 즉시 그 사실을 전달받고 싶어하는 객체
-
-import java.util.List;
-import java.util.ArrayList;
+//code.oa.gg/java8/1416
 
 class Main {
     public static void main(String[] args) {
-        View aButton = new Button();
-        // 홍길동 : 내가 너(뷰, 버튼)한테 나의 리모콘을 줄테니까, 너가 그것을 잘 가지고 있다가, 너가 클릭이 되면, 내 리모콘에 있는 버튼 중 하나를 눌러줘
-        // 홍길동 : 나의 리모콘에는 버튼이 2개 있는데, 그 중에서 onClick을 호출해야 되는데.. 너가 그걸 잘 처리할 수 있을까?
-        // 뷰 : 그건 걱정하지 않아도 됩니다. 당신이 나에게 준 홍길동 리모콘은, 어차피 onClick 버튼 빼고 모든 버튼이 제거됩니다. 즉 리모콘 형변환이 일어납니다.
-        // 뷰 : 대신 당신 해야할 일은, OnClickEventListen가 되어야 한다는 것 입나다.
-        // 홍길동 : 그럴줄 알고 그것(OnClickEventListen) 구현했습니다.
-        aButton.addOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(String msg) {
-                System.out.println("홍길동이 받은 메세지 : " + msg);
-            }
-        });
-        aButton.addOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(String msg) {
-                System.out.println("홍길순이 받은 메세지 : " + msg);
-            }
-        });
-        aButton.fireClickEvent();
-        aButton.fireClickEvent();
+        Activity activity = new MainActivity();
+        activity.onCreate();
+        // 출력 => 메인 액티비티가 실행되었습니다.
     }
 }
-class View {
 
-    /* 수정가능지역시작 */
-    List<OnClickListener> list = new ArrayList<>();
-
-    public void addOnClickListener(OnClickListener listener) {
-        list.add(listener);
+class Activity{
+    void onCreate(){
+        System.out.println("메인 액티비티가 실행되었습니다.");
     }
-
-    public void fireClickEvent(){
-        for(OnClickListener o : list){
-            o.onClick("뷰가 클릭되었습니다.");
-        }
-    }
-    /* 수정가능지역끝 */
 }
 
-class Button extends View {
+class MainActivity extends Activity{
 
-}
-
-interface OnClickListener {
-    public void onClick(String msg);
 }
