0d5eaf3054e9d26da82b9f4d1bdf20bb7e6532c0
kimdonghyeon <sh2115521@naver.com>
2022-07-27 09:42:33 +0900

    setOnClickListener 메서드 구현, 작업 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,59 +10,52 @@ abstract class Activity {
     abstract public void onCreate();
 }
 
-class MainActivity extends Activity {
+class MainActivity extends Activity implements View.OnClickListener{
     @Override
     public void onCreate() {
         System.out.println("메인 액티비티가 실행되었습니다.");
 
         // 아래 코드가 실행되도록 해주세요.
-        View.OnClickListener a = new 사과();
         View aButton = new Button(this);
-        aButton.setOnClickListener(a);
+        aButton.setOnClickListener(this);
 
         aButton.fireClickEvent();
-        // 출력 : 사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
-        // 조건 : 위 문구는 사과 클래스 안의 메서드에 의해서 출력되어야 합니다.
+        // 출력 : MainActivity는 버튼이 클릭되었다는 사실을 방금 알았습니다.!
+        // 조건 : 위 문구는 MainActivity 클래스 안의 메서드에 의해서 출력되어야 합니다.
+    }
+
+
+    @Override
+    public void clicked() {
+        System.out.println("메인 엑티비티가 클릭되었습니다.");
     }
 }
 
 abstract class View {
-    public void fireClickEvent() {
-    }
+    private OnClickListener onClickListener;
+    private Activity activity;
 
     static interface OnClickListener {
-
-        void fireClickEvent();
+        public void clicked();
     }
 
-    void setOnClickListener(OnClickListener onClickListener) {
-
-    }
-}
-
-class Button extends View {
-
-    OnClickListener onClickListener;
-
-    Button(Activity activity) {
-    }
-
-    @Override
     void setOnClickListener(OnClickListener onClickListener) {
         this.onClickListener = onClickListener;
     }
 
-    @Override
-    public void fireClickEvent() {
-        onClickListener.fireClickEvent();
+    void fireClickEvent() {
+        System.out.println("== 버튼이 클릭되었습니다. ==");
+        System.out.println("구독자에게 알람을 합니다.");
+
+        this.onClickListener.clicked();
     }
 }
 
-class 사과 implements View.OnClickListener {
-    @Override
-    public void fireClickEvent() {
-        System.out.println("사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
+class Button extends View{
+
+    Button(Activity activity) {
     }
 }
+
 
 
